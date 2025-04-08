beefdf1b3d04ab8d8bfd86068db79dbec9d9ce11
Majok-Bol <bolmajok001@gmail.com>
2025-04-08 17:56:06 +0300

    added background color to home page

--- lib/main.dart
+++ lib/main.dart
@@ -17,15 +17,17 @@ class TopNavigationBar extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return DefaultTabController(length: 3, child:Scaffold(
+      backgroundColor: Colors.grey.shade400,
       appBar: AppBar(
         backgroundColor: Colors.blueGrey.shade500,
         bottom: TabBar(tabs: [
-          Tab(child: Text('Status',style: TextStyle(color: Colors.white),),),
+          Tab(child: Text('Status',style: TextStyle(color: Colors.white,),),),
           Tab(child: Text('Calls',style: TextStyle(color: Colors.white),),),
           Tab(child: Text('Chats',style: TextStyle(color: Colors.white),),),
         ]),
       ),
-      body:TabBarView(children:[
+      body:TabBarView(
+          children:[
         Center(child: Text('Status Screen'),),
         Center(child: Text('Calls Screen'),),
         Center(child: Text('Chats Screen'),),
